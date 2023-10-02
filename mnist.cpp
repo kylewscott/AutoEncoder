82593fdedef2a0a615010aa945bd7a8b42745332
kyle <kylewscott512@gmail.com>
2023-10-02 17:16:57 -0500

    Finished Back Propagation

--- mnist.cpp
+++ mnist.cpp
@@ -6,29 +6,28 @@
 #include <random>
 #include <C:\eigen-3.4.0\Eigen\Dense>
 
-using namespace std;    
 using namespace Eigen;
 
 //Function to load in data as 1d vector
 int numRow;
-vector<double> loadData(string filePath){
+std::vector<double> loadData(std::string filePath){
     //open file
-    ifstream nums;
+    std::ifstream nums;
     nums.open(filePath);
 
     //Take in the first row of labels
-    string str;
+    std::string str;
     getline(nums, str);
 
     //1d vector to store entire mnist dataset
-    vector<double> data;
+    std::vector<double> data;
 
     //add mnist data to data
     while(nums >> str){
         int temp;
-        stringstream s(str);
+        std::stringstream s(str);
         while(s >> temp){
-            string stemp;
+            std::string stemp;
             double number = temp / 255.0; //normalize data
             data.push_back(number);
             getline(s, stemp, ',');
@@ -41,24 +40,24 @@ vector<double> loadData(string filePath){
 }
 
 //function to load in labels as 1d vector
-vector<double> loadLabels(string filepath){
+std::vector<double> loadLabels(std::string filepath){
     //open file
-    ifstream label;
+    std::ifstream label;
     label.open(filepath);
 
     //take in first row
-    string str;
+    std::string str;
     getline(label, str);
 
     //create output vector
-    vector<double> labels;
+    std::vector<double> labels;
 
     //add each label into label
     while(label >> str){
         int temp;
-        stringstream s(str);
+        std::stringstream s(str);
         while(s >> temp){
-            string stemp;
+            std::string stemp;
             double number = temp;
             labels.push_back(number);
             getline(s, stemp);
@@ -67,30 +66,34 @@ vector<double> loadLabels(string filepath){
     return labels;
 }
 
+//Possible change to void functions?
 //Activation functions
-MatrixXd sigmoid(MatrixXd x){
-    for(int i = 0; i < x.size(); i++){
+Eigen::VectorXd sigmoid(Eigen::VectorXd x){
+    // try to do this in-place using an Array view of the matrix
+    //x.array().exp().addTo(1).inverse();
+
+    for(size_t i = 0; i < x.size(); i++){
         x(i) = (1/(1+exp(-x(i))));
     }
     return x;
 }
 
-MatrixXd sigmoidPrime(MatrixXd x){
-    for(int i = 0; i < x.size(); i++){
+Eigen::VectorXd sigmoidPrime(Eigen::VectorXd x){
+    for(size_t i = 0; i < x.size(); i++){
         x(i) = x(i) * (1-x(i));
     }
     return x;
 }
 
-MatrixXd relu(MatrixXd x){
-    for(int i = 0; i < x.size(); i++){
-        x(i) = max(0.0, x(i));
+Eigen::VectorXd relu(Eigen::VectorXd x){
+    for(size_t i = 0; i < x.size(); i++){
+        x(i) = std::max(0.0, x(i));
     }
     return x;
 }
 
-MatrixXd reluPrime(MatrixXd x){
-    for(int i = 0; i < x.size(); i++){
+Eigen::VectorXd reluPrime(Eigen::VectorXd x){
+    for(size_t i = 0; i < x.size(); i++){
         if(x(i) < 0){
             x(i) = 0;
         }
@@ -101,122 +104,89 @@ MatrixXd reluPrime(MatrixXd x){
     return x;
 }
 
-MatrixXd softMax(MatrixXd x){
+Eigen::VectorXd softMax(Eigen::VectorXd x){
     double sum;
-    for(int i = 0; i < x.size(); i++){
+    for(size_t i = 0; i < x.size(); i++){
         sum += exp(x(i));
     }
-    for(int i = 0; i < x.size(); i++){
+    for(size_t i = 0; i < x.size(); i++){
         x(i) = exp(x(i)) / sum;
     }
     return x;
 }
 
-MatrixXd softMaxPrime(MatrixXd x){
-    MatrixXd softmax = softMax(x);
-    MatrixXd derivative;
-    for(int i = 0; i < softmax.size(); i++){
-        x(i) = softmax(i) * (1 - softmax(i));   
+Eigen::MatrixXd softMaxPrime(Eigen::VectorXd x){
+    Eigen::VectorXd softmax = softMax(x);
+    Eigen::MatrixXd derivative(10,10);
+    for(size_t i =0; i < softmax.size(); i++){
+        for(size_t j = 0; j < softmax.size(); j++){
+            if(i == j){
+                derivative(i, j) = softmax(i) * (1 - softmax(i));
+            }
+            else{
+                derivative(i, j) = softmax(i) * softmax(j);
+            }
+        }
     }
-    return x;
+    return derivative;
 }
 
 //Autoencoder
-void autoencoder(MatrixXd m, MatrixXd target, double learningRate, int epochs){
-    //Create Random number generator 0 - 1
-    random_device rand;
-    mt19937 gen(rand());
-    uniform_real_distribution<double> dis(0.0, 1.0);
-    //Create weights for each layer
-    MatrixXd weight1(128, 784);
-    for(int i = 0; i < 128; i++){
-        for(int j = 0; j < 784; j++){
-            weight1(i, j) = dis(gen);
-        }
-    }
-    MatrixXd weight2(64, 128);
-    for(int i = 0; i < 64; i++){
-        for(int j = 0; j < 128; j++){
-            weight2(i, j) = dis(gen);
-        }
-    }
-    MatrixXd weight3(10, 64);
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 64; j++){
-            weight3(i, j) = dis(gen);
-        }
-    }
-    //Create biases for each layer
-    MatrixXd bias1(128,1);
-    for(int i = 0; i < 128; i++){
-        bias1(i, 0) = dis(gen);
-    }
-    MatrixXd bias2(64,1);
-    for(int i = 0; i < 64; i++){
-        bias2(i, 0) = dis(gen);
-    }
-    MatrixXd bias3(10,1);
-    for(int i = 0; i < 10; i++){
-        bias3(i, 0) = dis(gen);
-    }
+void autoencoder(Eigen::MatrixXd m, Eigen::VectorXd target, double learningRate, int epochs){
+    //Create Random weights and biases 0-1
+    Eigen::MatrixXd weight1 = Eigen::MatrixXd::Random(128, 784);
+    weight1 = weight1.array().abs();
 
-    for(int i = 0; i < 1; i++){
-        //Forward propagate
-        //input layer size of 784
-        MatrixXd a0 = m;
-        //input to hidden layer 1 size of 128
-        MatrixXd z1 = (weight1 * a0) + bias1;
-        MatrixXd a1 = sigmoid(z1);
-        //input to hidden layer 2 size of 64
-        MatrixXd z2 = (weight2 * a1) + bias2;
-        MatrixXd a2 = sigmoid(z2);
-        //input to output size of 10
-        MatrixXd z3 = (weight3 * a2) + bias3;
-        MatrixXd a3 = softMax(z3);
+    Eigen::MatrixXd weight2 = Eigen::MatrixXd::Random(64, 128);
+    weight2 = weight2.array().abs();
 
-        //backward propagate
-        MatrixXd error = a3 - target;
-        MatrixXd delta1(10,1);
-        for(int i = 0; i < 10; i++){
-            delta1(i) = error(i) * softMaxPrime(z3)(i);
-        }
-        MatrixXd deltaW3 = delta1 * a2.transpose();
-        MatrixXd deltaB3 = delta1;
+    Eigen::MatrixXd weight3 = Eigen::MatrixXd::Random(10, 64);
+    weight3 = weight3.array().abs();
 
-        MatrixXd temp = weight3.transpose() * delta1;
-        MatrixXd delta2(64,1);
-        for(int i = 0; i < 64; i++){
-            delta2(i) = temp(i) * sigmoidPrime(z2)(i);
-        }
-        MatrixXd deltaW2 = delta2 * a1.transpose();
-        MatrixXd deltaB2 = delta2;
-        
-        temp = weight2.transpose() * delta2;
-        MatrixXd delta3(128,1);
-        for(int i = 0; i < 128; i++){
-            delta3(i) = temp(i) * sigmoidPrime(z1)(i);
-        }
-        MatrixXd deltaW1 = delta3 * a0.transpose();
-        MatrixXd deltaB1 = delta3;
+    Eigen::VectorXd bias1 = Eigen::VectorXd::Random(128);
+    bias1 = bias1.array().abs();
 
-        //update weights and biases
-        weight3 = weight3 + learningRate * a2.transpose() * deltaW3;
-        bias3 = bias3 + learningRate * deltaB3;
+    Eigen::VectorXd bias2 = Eigen::VectorXd::Random(64);
+    bias2 = bias2.array().abs();
 
-        weight2 = weight2 + learningRate * a1.transpose() * deltaW2;
-        bias2 = bias2 + learningRate * deltaB2;
+    Eigen::VectorXd bias3 = Eigen::VectorXd::Random(10);
+    bias3 = bias3.array().abs();
 
-        weight1 = weight1 + learningRate * a0 * deltaW1;
-        bias1 = bias1 + learningRate * deltaB1;
+    //Forward propagate
+    //input to hidden layer 1 size of 128
+    Eigen::VectorXd z1 = (weight1 * m) + bias1;
+    Eigen::VectorXd a1 = sigmoid(z1);
+    //input to hidden layer 2 size of 64
+    Eigen::VectorXd z2 = (weight2 * a1) + bias2;
+    Eigen::VectorXd a2 = sigmoid(z2);
+    //input to output size of 10
+    Eigen::VectorXd z3 = (weight3 * a2) + bias3;
+    Eigen::VectorXd a3 = softMax(z3);
 
-        double totalError = error.sum();
-    }
+    //backward propagate
+    Eigen::VectorXd error = a3 - target;
+    Eigen::VectorXd delta1 = softMaxPrime(a3) * error; //10x1
+    Eigen::VectorXd delta2 = (weight3.transpose() * delta1).array() * sigmoidPrime(a2).array(); //64x1
+    Eigen::VectorXd delta3 = (weight2.transpose() * delta2).array() * sigmoidPrime(z1).array(); //128x1
+
+    //update weights and biases
+    weight3 = (weight3.array() + learningRate).array() * (delta1 * a2.transpose()).array(); //10x64
+    bias3 = (bias3.array() + learningRate).array() * delta1.array();    //10x1
+
+    weight2 = (weight2.array() + learningRate).array() * (delta2 * a1.transpose()).array();  //64x128
+    bias2 = (bias2.array() + learningRate).array() * delta2.array();   //64x1
+
+    weight1 = (weight1.array() + learningRate).array() * (delta3 * m.transpose()).array();  //128x784
+    bias1 = (bias1.array() + learningRate).array() * delta3.array();   //128x1
+
+    //Calculate total error
+    double totalError = std::sqrt(error.array().square().sum());
 
 }
 
 //Function to put specified digit into digit.csv 
-void plotDigitInput(MatrixXd m, int index){
-    ofstream outputfile("digit.csv", ios::out);
+void plotDigitInput(Eigen::MatrixXd m, int index){
+    std::ofstream outputfile("digit.csv", std::ios::out);
     for(int i = 0; i < 784; i ++){
         outputfile << m.coeff(i, index) << ",";
     }
@@ -225,24 +195,26 @@ void plotDigitInput(MatrixXd m, int index){
 
 int main() {
     //load mnist data and labels
-    vector<double> data = loadData("mnist_train.csv");
-    vector<double> labels = loadLabels("mnist_train_targets.csv");
+    std::vector<double> data = loadData("mnist_train.csv");
+    std::vector<double> labels = loadLabels("mnist_train_targets.csv");
     int value = labels[25];
-    MatrixXd target(10, 1);
-    target = MatrixXd::Zero(10,1);
+    Eigen::VectorXd target(10, 1);
+    target = Eigen::VectorXd::Zero(10,1);
     target(value,0) = 1.0;
-    //cout << target;
 
     //Turn data into a matrix and print out
-    MatrixXd matrix = Map<Matrix<double, Dynamic, Dynamic, RowMajor>>(data.data(), numRow, data.size() / numRow);
+    Eigen::MatrixXd matrix = Eigen::Map<Eigen::Matrix<double, Dynamic, Dynamic, RowMajor>>(data.data(), numRow, data.size() / numRow);
 
     //Call autoencoder function with mnist dataset
     autoencoder(matrix.col(1), target, 0.01, 10);
     //put specidifed digit into digit.csv
-    //cout << labels[25] << "\n\n";
     plotDigitInput(matrix, 25);
     
-
     return 0;
 
 }
+
+// *** TO DO ***
+//Go through and take out unecessary lines
+//Look into lambdas if needed
+//Create classes for the layers and autoencoder
