270ca3d703917161a343fd73a038989b3d7bf05c
Kyle Scott <kylewscott512@gmail.com>
2023-11-29 13:00:12 -0500

    comment to digitPLot

--- digitPlot.py
+++ digitPlot.py
@@ -5,11 +5,9 @@ import csv
 data = []
 with open('digit.csv', mode='r') as file:
     reader = csv.reader(file)
-    print(reader)
     for row in reader:
         for i in range(len(row)-1):
             data.append(float(row[i]))
-print(data)
 
 #Reshape into 28x28 matrix
 datax = np.reshape(data, (28,28))
